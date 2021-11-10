1b350b49138ca7f208a947547d6cd30ecfc05336
Danny Lin <danny@kdrag0n.dev>
2021-11-09 19:48:30 -0800

    commands: Add command to diff props

---
+++ src/blobs/props.ts
@@ -0,0 +1,78 @@
+import { promises as fs } from 'fs'
+
+import { exists } from '../util/fs'
+import { parseLines } from '../util/parse'
+import { ALL_PARTITIONS } from '../util/partitions'
+
+export type PartitionProps = Map<string, Map<string, string>>
+
+export interface PropChanges {
+  added: Map<string, string>
+  modified: Map<string, Array<string>>
+  removed: Map<string, string>
+}
+
+export function parseProps(file: string) {
+  let props = new Map<string, string>()
+  for (let line of parseLines(file)) {
+    let [key, value] = line.split('=', 2)
+    props.set(key, value)
+  }
+
+  return props
+}
+
+export async function loadPartitionProps(sourceRoot: string) {
+  let partProps = new Map<string, Map<string, string>>() as PartitionProps
+
+  for (let partition of ALL_PARTITIONS) {
+    let propPath = `${sourceRoot}/${partition}/build.prop`
+    if (partition == 'system' && !(await exists(propPath))) {
+      // System-as-root
+      propPath = `${sourceRoot}/system/system/build.prop`
+    }
+    if (!(await exists(propPath))) {
+      continue
+    }
+
+    let props = parseProps(await fs.readFile(propPath, { encoding: 'utf8' }));
+    partProps.set(partition, props)
+  }
+
+  return partProps
+}
+
+export function diffPartitionProps(partPropsRef: PartitionProps, partPropsNew: PartitionProps) {
+  let partChanges = new Map<string, PropChanges>()
+  for (let [partition, propsNew] of partPropsNew.entries()) {
+    let propsRef = partPropsRef.get(partition)
+    let changes = {
+      added: new Map<string, string>(),
+      modified: new Map<string, Array<string>>(),
+      removed: new Map<string, string>(),
+    } as PropChanges
+
+    // Added, modified
+    for (let [newKey, newValue] of propsNew.entries()) {
+      if (propsRef?.has(newKey)) {
+        let refValue = propsRef.get(newKey)!
+        changes.modified.set(newKey, [refValue, newValue])
+      } else {
+        changes.added.set(newKey, newValue)
+      }
+    }
+
+    // Removed
+    if (propsRef != null) {
+      for (let [refKey, refValue] of propsRef.entries()) {
+        if (!propsNew.has(refKey)) {
+          changes.removed.set(refKey, refValue)
+        }
+      }
+    }
+
+    partChanges.set(partition, changes)
+  }
+
+  return partChanges
+}
---
+++ src/commands/diff-props.ts
@@ -0,0 +1,73 @@
+import { Command, flags } from '@oclif/command'
+import * as chalk from 'chalk'
+
+import { diffPartitionProps, loadPartitionProps, PartitionProps } from '../blobs/props'
+
+const BUILD_KEY_PATTERN = /^ro(?:system_ext|product|vendor)?\.build\./
+
+function removeBuildProps(partProps: PartitionProps) {
+  for (let props of partProps.values()) {
+    for (let key of props.keys()) {
+      if (key.match(BUILD_KEY_PATTERN)) {
+        props.delete(key)
+      }
+    }
+  }
+}
+
+function forEachPropLine(props: Map<string, string>, callback: (prop: string) => void) {
+  for (let [key, value] of props.entries()) {
+    callback(`${key}=${chalk.bold(value)}`)
+  }
+}
+
+function forEachPropLineModified(
+  props: Map<string, Array<string>>,
+  callback: (prop: string) => void,
+) {
+  for (let [key, [refValue, newValue]] of props.entries()) {
+    callback(`${key}=${chalk.bold(refValue)} -> ${chalk.bold(chalk.blue(newValue))}`)
+  }
+}
+
+export default class DiffProps extends Command {
+  static description = 'find missing and different properties compared to a reference system'
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    all: flags.boolean({char: 'a', description: 'show all differences, not only missing props', default: false}),
+    includeBuild: flags.boolean({char: 'b', description: 'include build props', default: false}),
+  }
+
+  static args = [
+    {name: 'sourceRef', description: 'path to root of reference system', required: true},
+    {name: 'sourceNew', description: 'path to root of new system', required: true},
+  ]
+
+  async run() {
+    let {flags: {all, includeBuild}, args: {sourceRef, sourceNew}} = this.parse(DiffProps)
+
+    let propsRef = await loadPartitionProps(sourceRef)
+    let propsNew = await loadPartitionProps(sourceNew)
+
+    // Remove build props?
+    if (!includeBuild) {
+      removeBuildProps(propsRef)
+      removeBuildProps(propsNew)
+    }
+
+    let partChanges = diffPartitionProps(propsRef, propsNew)
+
+    for (let [partition, changes] of partChanges.entries()) {
+      this.log(chalk.bold(partition))
+
+      forEachPropLine(changes.removed, p => this.log(chalk.red(`    ${p}`)))
+      if (all) {
+        forEachPropLine(changes.added, p => this.log(chalk.green(`    ${p}`)))
+        forEachPropLineModified(changes.modified, p => this.log(`    ${p}`))
+      }
+
+      this.log()
+    }
+  }
+}
