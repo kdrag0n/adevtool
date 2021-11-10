0dd57b0777fa19dfe87a29d254ae3bfd94234ac2
Danny Lin <danny@kdrag0n.dev>
2021-11-09 19:49:22 -0800

    check-presigned: Refactor logic into blobs/

---
+++ src/blobs/presigned.ts
@@ -0,0 +1,73 @@
+import { promises as fs } from 'fs'
+import * as path from 'path'
+import { $ } from 'zx'
+
+import { parseSeappContexts } from '../sepolicy/seapp'
+import { listFilesRecursive } from '../util/fs'
+import { BlobEntry } from './entry'
+
+export async function parsePresignedRecursive(sepolicyDirs: Array<string>) {
+  let contexts = []
+  for (let dir of sepolicyDirs) {
+    for await (let file of listFilesRecursive(dir)) {
+      if (path.basename(file) != 'seapp_contexts') {
+        continue
+      }
+
+      let rawContexts = await fs.readFile(file, { encoding: 'utf8' })
+      contexts.push(...parseSeappContexts(rawContexts))
+    }
+  }
+
+  return new Set(contexts.filter(c => c.seinfo != 'platform')
+    .map(c => c.name))
+}
+
+export async function updatePresignedBlobs(
+  aapt2Path: string,
+  source: string,
+  presignedPkgs: Set<string>,
+  entries: Iterable<BlobEntry>,
+  entryCallback?: (entry: BlobEntry) => void,
+) {
+  let updatedEntries = []
+  for (let entry of entries) {
+    if (path.extname(entry.path) != '.apk') {
+      continue
+    }
+
+    if (entryCallback != undefined) {
+      entryCallback(entry)
+    }
+
+    let procOut = await $`${aapt2Path} dump packagename ${source}/${entry.srcPath}`
+    let pkgName = procOut.stdout.trim()
+    if (presignedPkgs.has(pkgName)) {
+      entry.isPresigned = true
+      updatedEntries.push(entry)
+    }
+  }
+
+  return updatedEntries
+}
+
+export async function enumeratePresignedBlobs(
+  aapt2Path: string,
+  source: string,
+  presignedPkgs: Set<string>,
+) {
+  let presignedPaths = []
+  for await (let file of listFilesRecursive(source)) {
+    if (path.extname(file) != '.apk') {
+      continue
+    }
+
+    let procOut = await $`${aapt2Path} dump packagename ${file}`
+    let pkgName = procOut.stdout.trim()
+    if (presignedPkgs.has(pkgName)) {
+      presignedPaths.push(file)
+    }
+  }
+
+  return presignedPaths
+}
--- src/commands/check-presigned.ts
+++ src/commands/check-presigned.ts
@@ -1,12 +1,9 @@
 import { Command, flags } from '@oclif/command'
 import { promises as fs } from 'fs'
-import * as path from 'path'
 import * as chalk from 'chalk'
-import { $ } from 'zx'
 
 import { parseFileList } from '../blobs/file_list'
-import { listFilesRecursive } from '../util/fs'
-import { parseSeappContexts } from '../sepolicy/seapp'
+import { enumeratePresignedBlobs, parsePresignedRecursive, updatePresignedBlobs } from '../blobs/presigned'
 
 export default class CheckPresigned extends Command {
   static description = 'check for APKs that should be presigned'
@@ -14,7 +11,7 @@ export default class CheckPresigned extends Command {
   static flags = {
     help: flags.help({char: 'h'}),
     aapt2: flags.string({char: 'a', description: 'path to aapt2 executable', default: 'out/host/linux-x86/bin/aapt2'}),
-    sepolicy: flags.string({char: 's', description: 'paths to device and vendor sepolicy dirs', required: true, multiple: true}),
+    sepolicy: flags.string({char: 'p', description: 'paths to device and vendor sepolicy dirs', required: true, multiple: true}),
   }
 
   static args = [
@@ -23,7 +20,7 @@ export default class CheckPresigned extends Command {
   ]
 
   async run() {
-    let {flags: {aapt2, sepolicy}, args: {source, listPath}} = this.parse(CheckPresigned)
+    let {flags: {aapt2: aapt2Path, sepolicy: sepolicyDirs}, args: {source, listPath}} = this.parse(CheckPresigned)
 
     // Parse list
     this.log(chalk.bold(chalk.greenBright('Parsing list')))
@@ -31,49 +28,18 @@ export default class CheckPresigned extends Command {
     let entries = list != null ? parseFileList(list) : null
 
     // Find and parse sepolicy seapp_contexts
-    let contexts = []
-    for (let dir of sepolicy) {
-      for await (let file of listFilesRecursive(dir)) {
-        if (path.basename(file) != 'seapp_contexts') {
-          continue
-        }
-
-        let rawContexts = await fs.readFile(file, { encoding: 'utf8' })
-        contexts.push(...parseSeappContexts(rawContexts))
-      }
-    }
-    let presignedPkgs = new Set(contexts.filter(c => c.seinfo != 'platform')
-      .map(c => c.name))
+    let presignedPkgs = await parsePresignedRecursive(sepolicyDirs)
 
     if (entries != null) {
       // Get APKs from blob entries
-      for (let entry of entries) {
-        if (path.extname(entry.path) != '.apk') {
-          continue
-        }
-
-        let procOut = await $`${aapt2} dump packagename ${source}/${entry.srcPath}`
-        let pkgName = procOut.stdout.trim()
-        if (presignedPkgs.has(pkgName)) {
-          entry.isPresigned = true
-          this.log(entry.srcPath)
-        }
-      }
+      let presignedEntries = await updatePresignedBlobs(aapt2Path, source, presignedPkgs, entries)
+      presignedEntries.forEach(e => this.log(e.srcPath))
 
       // TODO: write new list
     } else {
       // Find APKs
-      for await (let file of listFilesRecursive(source)) {
-        if (path.extname(file) != '.apk') {
-          continue
-        }
-
-        let procOut = await $`${aapt2} dump packagename ${file}`
-        let pkgName = procOut.stdout.trim()
-        if (presignedPkgs.has(pkgName)) {
-          this.log(file)
-        }
-      }
+      let presignedPaths = await enumeratePresignedBlobs(aapt2Path, source, presignedPkgs)
+      presignedPaths.forEach(p => this.log(p))
     }
   }
 }
