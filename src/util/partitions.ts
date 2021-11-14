fe09189d55252bc7797b7f267eaa86a1b2c61037
Danny Lin <danny@kdrag0n.dev>
2021-11-14 01:49:14 -0800

    partitions: Organize partitions

--- src/util/partitions.ts
+++ src/util/partitions.ts
@@ -1,14 +1,20 @@
-// Excluding system
-export const EXT_PARTITIONS = new Set([
-  // Android system
+// Android system partitions, excluding "system"
+export const EXT_SYS_PARTITIONS = new Set([
   'system_ext',
   'product',
   'vendor',
   'odm',
+])
 
-  // GKI DLKMs
+// GKI DLKM partitions
+export const DLKM_PARTITIONS = new Set([
   'vendor_dlkm',
   'odm_dlkm',
+])
+
+export const EXT_PARTITIONS = new Set([
+  ...EXT_SYS_PARTITIONS,
+  ...DLKM_PARTITIONS,
 ])
 
 // All partitions
