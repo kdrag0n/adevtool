a644a921c9a6adec1483bf38ffbe32e7eddab9f7
Danny Lin <danny@kdrag0n.dev>
2021-11-21 19:44:03 -0800

    util: fs: Add temporary directory and mountpoint management

--- src/util/fs.ts
+++ src/util/fs.ts
@@ -1,5 +1,15 @@
 import { promises as fs } from 'fs'
+import * as os from 'os'
 import * as path from 'path'
+
+import { run } from './process'
+
+const TMP_PREFIX = 'adevtool-'
+
+export interface TempState {
+  dir: string
+  mounts: Array<string>
+}
 
 // https://stackoverflow.com/a/45130990
 export async function* listFilesRecursive(dir: string): AsyncGenerator<string> {
@@ -21,5 +31,29 @@ export async function exists(path: string) {
   } catch {
     // Doesn't exist or can't read
     return false
+  }
+}
+
+export async function withTempDir<Return>(callback: (tmp: TempState) => Promise<Return>) {
+  let tmp = os.tmpdir()
+  let rand = `${Math.random()}`.slice(2)
+  let dir = `${tmp}/${TMP_PREFIX}${rand}`
+
+  await fs.rm(dir, { force: true, recursive: true })
+  await fs.mkdir(dir)
+  let state = {
+    dir: dir,
+    mounts: [],
+  } as TempState
+
+  try {
+    return await callback(state)
+  } finally {
+    // Clean up mountpoints
+    for (let mount of state.mounts) {
+      await run(`umount -f ${mount}`)
+    }
+
+    await fs.rm(dir, { force: true, recursive: true })
   }
 }
