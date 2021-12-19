44af21c88536af3b64ec69227d6affa625e36ed2
Danny Lin <danny@kdrag0n.dev>
2021-12-18 18:25:54 -0800

    frontend: Add parallel for-each-device helper with CLI output

---
+++ src/frontend/devices.ts
@@ -0,0 +1,28 @@
+import chalk from "chalk"
+
+export async function forEachDevice<Device>(
+  devices: Device[],
+  parallel: boolean,
+  callback: (device: Device) => Promise<void>,
+  deviceKey: (device: Device) => string = d => d as any as string,
+) {
+  let jobs = []
+  let isMultiDevice = devices.length > 1
+  for (let device of devices) {
+    if (isMultiDevice) {
+      console.log(`
+
+${chalk.bold(chalk.blueBright(deviceKey(device)))}
+`)
+    }
+
+    let job = callback(device)
+    if (parallel) {
+      jobs.push(job)
+    } else {
+      await job
+    }
+  }
+
+  await Promise.all(jobs)
+}
